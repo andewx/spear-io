28896815d2212ae5ee6dfdb7ccfc4e44269d2fe0
Brian Anderson <brian.michael.anderson@gmail.com>
2025-12-28 02:07:25 +0000

    Fix import statement for httpCommand

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -5,7 +5,7 @@
 
 import type { Request, Response } from 'express';
 import type { TAPIResponse } from '../types/index.js';
-import {httpCommand) from '../index.js';
+import httpCommand from '../index.js';
 
 export class MainController {
   /**
